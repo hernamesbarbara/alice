c4c241d2c6ee72c4fbb0624978047671caa33541
austin <austinogilvie@gmail.com>
2017-03-09 12:07:56 -0500

    replaced len check w/ more robust stop words

--- alice.py
+++ alice.py
@@ -3,6 +3,10 @@
 """alice.py
 """
 from string import punctuation
+from nltk.corpus import stopwords
+# set stopwords to the built in list from nltk
+# check for them in your count_words func to rm stop words w/ little lexical diversity
+STOP_WORDS = set(stopwords.words('english'))
 
 def count_words(txt_or_file):
     try:
@@ -12,7 +16,7 @@ def count_words(txt_or_file):
     txt = "".join(ch for ch in txt if ch not in punctuation).lower()
     wc = {}
     for word in txt.split():
-        if len(word)>3:
+        if word not in STOP_WORDS:
             if word not in wc:
                 wc[word] = 1
             else:
